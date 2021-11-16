95c067fb02c0035c2aa2d6a74300920441359ba6
pavan <48682313+pavan-aeturi@users.noreply.github.com>
2021-11-16 13:09:19 +0530

    Add files via upload

--- createModel.py
+++ createModel.py
@@ -6,15 +6,6 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-x_train /=255
-x_test /=255
-
-for train in range(len(x_train)):
-    for row in range(28):
-        for x in range(28):
-            if x_train[train][row][x] != 0:
-                x_train[train][row][x] = 1
-
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
